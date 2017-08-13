28a8c2ec28c797705eb2978c3e03c4c63344a309
PHUPATHSORN HARNPRAB <pharnpra@celestica.com>
2017-08-13 17:52:30 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'hhhhhh';
+  todoLists = [{task: '1.one', checked: false}, {task: '2. two', checked: true}, {task: '3. three', checked: true}];
+  newTask = '';
+
+  addTodoLists(newtask) {
+    this.todoLists.push({task: newtask, checked: false});
+    this.newTask = null;
+  }
+
+  checkList(task) {
+    
+  }
 }
